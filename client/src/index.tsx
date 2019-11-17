8b5f274a5dca90ba658858443730347953720aa8
Angelique Nehmzow <angelique@nehmzow.com>
2019-11-17 15:04:03 -0800

    simplify index, dont pass in props

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,4 +2,4 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { App } from './components/App';
 
-ReactDOM.render(<App compiler='TypeScript' framework='React' />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
