85ad95e0985a85e7e0570cce24af8fad18b072d4
Angelique Nehmzow <angelique@nehmzow.com>
2019-11-17 22:44:22 -0800

    styling trips

--- client/src/components/Trips.jsx
+++ client/src/components/Trips.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import styles from './styles/trips.css';
 
 export default function Trips({trips, addTrip, selectTrip}) {
   return (
-    <div>
-      <h3>Trips</h3>
-      <button onClick={addTrip}>Add trip</button>
-      {trips.map(trip => <div key={trip._id} tripid={trip._id} onClick={selectTrip}>{trip.tripName}</div>)}
+    <div className={styles.tripsContainer}>
+      <div className={styles.titleSection}>
+        <h3>Trips</h3>
+        <button className={styles.addTripButton} onClick={addTrip}>Add</button>
+      </div>
+      {trips.map(trip => {
+        return (
+          <div className={styles.tripContainer} key={trip._id} >
+            <div className={styles.trip} tripid={trip._id} onClick={selectTrip}>
+              {trip.tripName}
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
