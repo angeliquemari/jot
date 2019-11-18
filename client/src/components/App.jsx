1f84d22d22735e662c8f7493e39b2e6a2137681b
Angelique Nehmzow <angelique@nehmzow.com>
2019-11-17 21:32:10 -0800

    adding css reset file

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './styles/App.css';
+import styles from './styles/app.css';
 import $ from 'jquery';
 import Trips from './Trips';
 import Notes from './Notes';
