658aff93462e9b81d03fffaed1d9aa84b6c846de
Angelique Nehmzow <angelique@nehmzow.com>
2019-11-17 21:53:44 -0800

    add banner, add empty css files

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -106,6 +106,10 @@ export default class App extends React.Component {
   render() {
     return (
       <div className={styles.mainContainer}>
+        <div className={styles.header}>
+          <div className={styles.appTitle}>jot</div>
+          <div className={styles.appDescription}>Travel notes</div>
+        </div>
         <Trips trips={this.state.trips} addTrip={this.addTrip} selectTrip={this.selectTrip} />
         {this.state.selectedTrip !== undefined && <Notes notes={this.state.selectedTrip.notes} addNote={this.addNote} selectNote={this.selectNote} />}
         {this.state.selectedTrip !== undefined && this.state.selectedNote !== undefined && <Note note={this.state.selectedNote} updateNote={this.updateNote} />}
